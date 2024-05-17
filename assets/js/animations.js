7bba16ed8623845cca77c67476ac962503d6cc57
mhosma <165294787+mhosma@users.noreply.github.com>
2024-05-17 11:11:55 -0400

    Add files via upload

---
+++ assets/js/animations.js
@@ -0,0 +1,96 @@
+// JavaScript Document
+
+            function reload1() {
+                document.getElementById('iframeid1').src += '';
+                
+            }
+            btn1.onclick = reload1;
+
+
+
+            function reload2() {
+                document.getElementById('iframeid2').src += '';
+                
+            }
+            btn2.onclick = reload2;
+
+
+
+            function reload3() {
+                document.getElementById('iframeid3').src += '';
+                
+            }
+            btn3.onclick = reload3;
+
+
+
+            function reload4() {
+                document.getElementById('iframeid4').src += '';
+                
+            }
+            btn4.onclick = reload4;
+
+
+
+            function reload5() {
+                document.getElementById('iframeid5').src += '';
+                
+            }
+            btn5.onclick = reload5;
+
+
+
+            function reload6() {
+                document.getElementById('iframeid6').src += '';
+                
+            }
+            btn6.onclick = reload6;
+
+
+
+
+            function reload7() {
+                document.getElementById('iframeid7').src += '';
+                
+            }
+            btn7.onclick = reload7;
+
+
+
+            function reload8() {
+                document.getElementById('iframeid8').src += '';
+                
+            }
+            btn8.onclick = reload8;
+
+
+
+            function reload9() {
+                document.getElementById('iframeid9').src += '';
+                
+            }
+            btn9.onclick = reload9;
+
+
+
+            function reload10() {
+                document.getElementById('iframeid10').src += '';
+                
+            }
+            btn10.onclick = reload10;
+
+
+
+            function reload11() {
+                document.getElementById('iframeid11').src += '';
+                
+            }
+
+            btn11.onclick = reload11;
+
+
+            function reload12() {
+                document.getElementById('iframeid12').src += '';
+                
+            }
+            btn12.onclick = reload12;
